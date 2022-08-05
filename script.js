4d43e371b0e5641049c3de6d06548fc766f598b5
nanami <70756480+oosawananami@users.noreply.github.com>
2022-08-05 16:02:55 +0900

    ボタン追加

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ $(function(){
   $("header").load("header.html");
   $("footer").load("footer.html");
   // alert("描画完了");
+  const price =  $('p').data(price);
+  console.log(price);
+});
 
+
+$(function add(){
+  alert("カートに追加しました");
 });
