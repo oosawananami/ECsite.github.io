c6404a9c684f072d3fab0d6c879ef80b4ef82435
nanami <h_daniel_min@yahoo.co.jp>
2022-08-12 14:12:51 +0900

    0811

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const quantity = $('#quantity').get(0);
 $(function(){
   $("header").load("header.html");
   $("footer").load("footer.html");
@@ -10,32 +9,4 @@ $('.content_wrap').on('click',function(){
   const i_price = $(this).data('price');
 
   window.location.href = 'shop.html' + "?name=" + i_name + "&price=" + i_price ;
-});
-
-
-//商品の購入個数選択start
-$('#plus').on('click',function(){
-  quantity.value++
-});
-
-$('#minus').on('click',function(){
-  if(quantity.value >= 2){
-    quantity.value--
-  }else{
-    quantity.value = 1;
-  }
-});
-//商品の購入個数選択end
-
-//カートに追加
-var add = $('.add_cart')
-add.on('click',function (){
-  alert("カートに追加しました");
-
-});
-
-//お気に入りに追加
-var like = $('.add_like')
-like.on('click',function(){
-  alert("お気に入り");
 });
--- shop.js
+++ shop.js
@@ -24,3 +24,35 @@ $(function(){
   const result2= $('.producut_price').data('price');
   console.log(result2);
 });
+
+//商品の購入個数選択start
+const qtest = $('#quantity')
+const quantity = qtest.get(0);
+$('#plus').on('click',function(){
+  var sum = 1;
+  sum = quantity.value++;
+  console.log(sum);
+});
+
+$('#minus').on('click',function(){
+  if(quantity.value >= 2){
+    quantity.value--
+  }else{
+    quantity.value = 1;
+  }
+});
+//商品の購入個数選択end
+
+
+//カートに追加
+var add = $('.add_cart')
+add.on('click',function (){
+
+  console.log(quantity);
+});
+
+//お気に入りに追加
+var like = $('.add_like')
+like.on('click',function(){
+  alert("お気に入り");
+});
